1bbfd5177b023e92576ce006a015ef2f44b5cdd9
Vivek Tyagi <vivek.nstyagi@gmail.com>
2025-04-03 21:09:48 +0530

    Final cleanup of the code with rubocop fix

--- lib/string_calculator_kata.rb
+++ lib/string_calculator_kata.rb
@@ -6,20 +6,13 @@ class StringCalculatorKata
   def add
     return 0 if @input.empty?
 
-    delimiter = /,|\n/
-
-    if @input.start_with?("//")
-      delimiter_section, @input = @input.split("\n", 2)
-      delimiter = extract_delimiters(delimiter_section)
-    end
-
     numbers = @input.split(delimiter).map(&:to_i)
 
     negative_numbers = numbers.find_all(&:negative?)
 
     raise "Negatives not allowed: #{negative_numbers.join(', ')}" unless negative_numbers.blank?
 
-    numbers.reject{ |number| number > 1000 }.sum
+    numbers.reject { |number| number > 1000 }.sum
   end
 
   private
@@ -33,5 +26,17 @@ class StringCalculatorKata
     else
       Regexp.escape(delimiters[0])
     end
+  end
+
+
+  def delimiter
+    delimiter_exp = /,|\n/
+
+    if @input.start_with?("//")
+      delimiter_section, @input = @input.split("\n", 2)
+      delimiter_exp = extract_delimiters(delimiter_section)
+    end
+
+    delimiter_exp
   end
 end
